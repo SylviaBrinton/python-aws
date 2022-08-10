2a9f5a1d3e99d6490717e448a79bebaf9a4276ec
Sylvia Brinton <66237595+SylviaBrinton@users.noreply.github.com>
2022-08-10 16:06:24 +0530

    update code for create ec2

--- install-ec2.py
+++ install-ec2.py
@@ -1,12 +1,15 @@
 #Python Program for creating a connection
 import boto3
 
+#Function for connecting to EC2
 ec2 = boto3.client('ec2',
-				'us-east-1',
+				'ap-south-1',
 				aws_access_key_id='AKIAV2P2BS5DHCMQNNB7',
 				aws_secret_access_key='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')
 
-#This function will describe all the instances
-#with their current state
-response = ec2.describe_instances()
-print(response)
+#Function for running instances
+conn = ec2.run_instances(InstanceType="t2.micro",
+						MaxCount=1,
+						MinCount=1,
+						ImageId="ami-02d55cb47e83a99a0")
+print(conn)
